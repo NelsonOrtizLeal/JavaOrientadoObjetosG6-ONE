29e4a6c7f24075e5c5558a51194678c302563035
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-07-04 09:49:32 -0600

    Update: Asignando valores a las peliculas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidaEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(incluidaEnElPlan);
+        System.out.println(notaDeLaPelicula);
+
+        System.out.println(media);
     }
 }
