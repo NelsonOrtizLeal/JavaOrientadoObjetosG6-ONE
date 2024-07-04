841d7875b6e4113a4f4f68cbf444deeabf322f48
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-07-04 10:22:53 -0600

    Update: Dandole formato a los textos

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,19 @@ public class Main {
 
         double media = (8.2 + 6.0 + 9.0) / 3;
 
-        System.out.println(fechaDeLanzamiento);
-        System.out.println(incluidaEnElPlan);
-        System.out.println(notaDeLaPelicula);
+        System.out.printf("Lanzamiento: %d \nIncluida: %b \nNota: %.2f \nMedia: %.2f%n", fechaDeLanzamiento, incluidaEnElPlan, notaDeLaPelicula, media);
 
-        System.out.println(media);
+        System.out.println("Sinopsis de pelicula:");
 
         String sinopsis = """
                 Matrix es una paradoja
                 La mejor pelicula del fin del milenio
-                Fué lanzada en:\s
-               \s""" + fechaDeLanzamiento;
+                Fué lanzada en: %s
+               """.formatted(fechaDeLanzamiento);
 
         System.out.println(sinopsis);
 
         int clasificacion = (int) media / 2;
-        System.out.println(clasificacion);
+        System.out.printf("Califiacion: \n%d - 5 Estrellas",clasificacion);
     }
 }
