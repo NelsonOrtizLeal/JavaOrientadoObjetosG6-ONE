5ef2ee1e0fc1f6f8079eb93763fb3d44897139ac
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-07-08 22:39:38 -0600

    Update: Tomando decisiones #2

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -4,14 +4,21 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidaEnElPlan = true;
         double notaDeLaPelicula = 8.2;
-
         int anio = 2022;
+        String tipoPlan = "plus";
 
         // Condicional para mostrar un mensaje
         if(fechaDeLanzamiento >= anio){
             System.out.println("Peliculas más populares");
         }else{
             System.out.println("Película retro que aún vale la pena ver.");
+        }
+
+        // Condicional para permitirle ver a el usuario una pelicula
+        if(incluidaEnElPlan || tipoPlan.equals("plus")){
+            System.out.println("Disfrute su película");
+        }else{
+            System.out.println("Esta película no esta disponible para su plan actual.");
         }
     }
 }
